fb7dc2fa79a012c57248b5dc3c5be7572345b6a2
The Doctor <sciencedude100@yahoo.com>
2017-12-03 05:07:03 -0500

    updated serial protocol in Arduino sketch

--- Arduino/ArmControl/ArmControl.ino
+++ Arduino/ArmControl/ArmControl.ino
@@ -1,56 +1,44 @@
-#include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 
-LiquidCrystal_I2C disp(0x3f,20,4);
 Servo axis;
 int Position = 0;
+int Angle = 0;
+int Direction = 0;
 int ServoMax = 180;
 int ServoPin = 2;
+byte readMe[2] = {0,0};
 
 void setup() {
   Serial.begin(115200);
-  disp.init();
-  disp.backlight();
   axis.attach(ServoPin);
-  disp.setCursor(0,0);
-  disp.print("Ready to go!");
 }
 
 void loop() {
-  while(Serial.available() > 0){
-    disp.clear();
-    disp.setCursor(0,0);
-    disp.print("Recieved Serial Data");
-    int Direction = Serial.parseInt();
-    disp.setCursor(0,2);
-    int Angle = Serial.parseInt();
+  if (Serial.available() > 0){
+    Serial.readBytes(readMe, 2);
+    Direction = (int)readMe[0];
+    Angle = (int)readMe[1];
+    while(Serial.read() != 13);
     if (Direction == 0 && Position >= Angle){
       Position -= Angle;
       axis.write(Position);
       String output = "Moved -" + (String)Angle + " degrees";
-      disp.print(output);
       Serial.println(output);
     }else if (Direction == 1 && Position <= ServoMax-Angle){
       Position += Angle;
       axis.write(Position);
       String output = "Moved +" + (String)Angle + " degrees";
-      disp.print(output);
       Serial.println(output);
     }else if(Direction == 3){
-      disp.setCursor(0,2);
-      disp.print("Reset Servo");
       Serial.println("Reset Servo Position");
       axis.write(0);
       Position=0;
     }else{
-      disp.setCursor(0,2);
-      disp.print("Serial data invalid");
       Serial.println("Serial data not valid.");
+      Serial.println(Direction);
+      Serial.println(Angle);
     }
-    disp.setCursor(0,3);
     String Status = "Current Angle: " + (String)Position;
-    disp.print(Status);
     Serial.println(Status);
   }
 }
